4229fd8164d05f7af9a3689f4a673dff62b12a7d
Chris Bradford <nico.bar2012@gmail.com>
2020-09-28 16:15:44 +0200

    finish home

--- home.php
+++ home.php
@@ -16,46 +16,50 @@
 </head>
 
   <body>
-  <header>
-    <!-- Dropdown Structure -->
-    <ul id="dropdown1" class="dropdown-content">
+    <header>
+      <!-- Dropdown Structure -->
+      <ul id="dropdown1" class="dropdown-content">
       <li><a href="#!">Français</a></li>
       <li><a href="#!">Anglais</a></li>
       <!--<li class="divider"></li>
       <li><a href="#!">three</a></li>-->
-    </ul>
-    <nav>
-      <div class="nav-wrapper">
-        <a href="#" class="brand-logo center">Logo</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
-        </ul>
-        <ul id="nav-mobile" class="left hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-        </ul>
-      </div>
-    </nav>
+      </ul>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo center">Logo</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Panier</a></li>
+            <li><a href="badges.html">Login</a></li>
+            <li><a href="collapsible.html">Register</a></li>
+          </ul>
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="sass.html">Bibliothèque</a></li>
+          </ul>
+        </div>
+      </nav>
 
-    <ul id="slide-out" class="sidenav">
-    <li><div class="user-view">
-      <div class="background">
-        <img src="images/office.jpg">
-      </div>
-      <a href="#user"><img class="circle" src="images/yuna.jpg"></a>
-      <a href="#name"><span class="white-text name">John Doe</span></a>
-      <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
-    </div></li>
-    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-    <li><a href="#!">Second Link</a></li>
-    <li><div class="divider"></div></li>
-    <li><a class="subheader">Subheader</a></li>
-    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
-  </ul>
-  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul id="slide-out" class="sidenav">
+      <ul class="collection">
+      <li class="collection-item">Catégorie 1</li>
+      <li class="collection-item">Catégorie 2</li>
+      <li class="collection-item">Catégorie 3</li>
+      <li class="collection-item">Catégorie 4</li>
+      <li class="collection-item">Catégorie 5</li>
+      <li class="collection-item">Catégorie 6</li>
+      <li class="collection-item">Catégorie 7</li>
+      <li class="collection-item">...</li>
+      <li class="collection-item">Personaliser le sticker</li>
+      <li class="collection-item">SAV</li>
+      </ul>
+      </ul>
+      <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
-  </header>
+    </header>
+
+    <section class="hero">
+        <div class="hero-content">
+        </div>
+    </section>
 
 
   </body>
